5a005e4066a40cf10256da7e1cf9c6f1421169a5
kizunas <8s6s7e@gmail.com>
2024-01-05 16:14:09 +0900

    注釈の整理

--- app.ts
+++ app.ts
@@ -1,16 +1,13 @@
 function combine(
-  input1: number | string, 
+  input1: number | string, // Union型
   input2: number | string,
   resultConversion: 'as-number' | 'as-text', // UnionとLiteralを合わせたもの(指定されたもの以外、許可されない)
 ) {
   let result
-  if (
-    (typeof input1 === 'number' && typeof input2 === 'number') || 
-      resultConversion === 'as-number' 
-  ) {
+  if ((typeof input1 === 'number' && typeof input2 === 'number') || resultConversion === 'as-number') {
     result = +input1 + +input2; // 単項プラス演算子は、数値以外も数値へと変換（数値にできないものはNaNになるので注意）
   } else {
-    result = input1.toString() + input2.toString()
+    result = input1.toString() + input2.toString() // toStringで明示的に文字列を指定
   }
   return result;
   // if (resultConversion === 'as-number') {
@@ -22,13 +19,13 @@ function combine(
 }
 
 
-const combinedAges = combine(30, 26, 'as-number'); 
+const combinedAges = combine(30, 26, 'as-number');  // 結果をnumberで返してほしいためas-numberと記述
 console.log(combinedAges);
 
 const combinedStringAges = combine('30', '26', 'as-number'); // 文字列で渡すが数字で返してほしいという条件
 console.log(combinedStringAges);
 
 
-const combinedNames = combine('Max', 'Annna', 'as-text');
+const combinedNames = combine('Max', 'Annna', 'as-text'); // 結果をstringsで返してほしいためas-textと記述
 console.log(combinedNames);
 
