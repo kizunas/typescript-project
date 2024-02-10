50b23c021f21e455449d92cacdc8cb0bbaff4293
kizunas <8s6s7e@gmail.com>
2024-02-10 16:00:31 +0900

    エイリアス型修正

--- union-aliases.ts
+++ union-aliases.ts
@@ -1,3 +1,4 @@
+
 function combine(
   input1: number | string, // Union型
   input2: number | string,
@@ -29,3 +30,40 @@ console.log(combinedStringAges);
 const combinedNames = combine('Max', 'Annna', 'as-text'); // 結果をstringsで返してほしいためas-textと記述
 console.log(combinedNames);
 
+// エイリアス型
+// このtypeという記述はTypeScript
+// Combinableは型名
+// number | stringという書き方はUnion型の書き方
+type Combinable = number | string
+type ConversionDiscriptor = 'as-number' | 'as-text'
+
+function combine(
+  // input1: number | string, // Union型
+  // input2: number | string,
+  input1: Combinable, // エイリアス型
+  input2: Combinable, // エイリアス型
+  // resultConversion: 'as-number' | 'as-text', // UnionとLiteralを合わせたもの(指定されたもの以外、許可されない)
+  resultConversion: ConversionDiscriptor, // エイリアス型
+) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+  // if (resultConversion === 'as-number') {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
+}
+
+const combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('Max', 'Anna', 'as-text');
+console.log(combinedNames);
